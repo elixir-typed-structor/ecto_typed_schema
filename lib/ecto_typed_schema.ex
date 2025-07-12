04de84fdf96d59d50f17a46a2cf09841f164aa9f
Phil Chen <06fahchen@gmail.com>
2025-07-12 23:25:15 +0800

    first commit

---
+++ lib/ecto_typed_schema.ex
@@ -0,0 +1,18 @@
+defmodule EctoTypedSchema do
+  @moduledoc """
+  Documentation for `EctoTypedSchema`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoTypedSchema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_typed_schema_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoTypedSchemaTest do
+  use ExUnit.Case
+  doctest EctoTypedSchema
+
+  test "greets the world" do
+    assert EctoTypedSchema.hello() == :world
+  end
+end
